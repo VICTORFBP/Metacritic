860221d5d2bd2ae82100beb68a9cd05440383d76
VICTORFBP <victor.fbperez@gmail.com>
2025-03-20 18:51:55 -0500

    Se agrega libreria de iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import logo from "../assets/logoApp.png";// Asegúrate de tener este icono
+import logo from "../assets/logoApp.png";
 import "./Header.css";
-
+import {UserOutlined} from "@ant-design/icons"
+import { Button } from "antd";
 const Header = () => {
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
       <nav>
-        <a href="#movies">Movies</a>
-        <a href="#tvshows">TV Shows</a>
+        <a href="#movies">Peliculas</a>
+        <a href="#tvshows">Series</a>
       </nav>
       <div className="search-bar">
-        <input type="text" placeholder="Search here..." />
+        <input type="text" placeholder="Buscar Aqui..." />
         <button type="submit"></button>
       </div>
-      <img src="" alt="profile" className="profile-icon" />
+        <Button className="icoLogin" type="primary" icon={<UserOutlined />}></Button>
     </div>
   );
 };
